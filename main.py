7a6cce0f657d990322e1be37ced3ede4310adb53
Xamrey <92014847+Xamrey@users.noreply.github.com>
2025-03-20 12:30:16 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import json
+import random
 
 class Stage: # Allows me to reference what menu is being used easier
     MenuMain = "__STAGE-MENU-MAIN__"
@@ -12,12 +13,20 @@ class Stage: # Allows me to reference what menu is being used easier
 Stage = Stage() # Removing the need to create a copy of the class every time it is used
 
 class Asset:
-    def __init__(self, zIndex, asset, selectable): # zIndex should be between 0 and 5
+    def __init__(self, zIndex, asset, pos, selectable): # zIndex should be between 0 and 5
         self.zIndex = zIndex
         self.asset = asset
+        self.pos = pos
         self.selectable = selectable
 
-assets = []
+pygame.init()
+pygame.font.init()
+font = pygame.font.SysFont("Arial" ,24) # rename and change later (for various different text objects)
+wn = pygame.display.set_mode((500, 500)) # change later
+
+assets = [
+    Asset(0, font.render("sample text", True, (0, 0, 0)), (0, 0), False)
+]
 
 def checkSelected():
     return "change later"
@@ -40,18 +49,12 @@ saveData = True
 try:
     data = open("data.txt", "r")
     formatted = json.loads(data.read())
-    print(formatted)
+    practiceData = formatted
     data.close()
 except Exception as err:
     saveData = False
     print(f"Error Code: '{err}'")
     createNotif(True, "There was an error in loading your data, any progress you make will not be saved (error code has been printed to the console)")
-
-pygame.init()
-pygame.font.init()
-font = pygame.font.SysFont("Arial" ,24)
-assets.append(Asset(0, font.render("sample text", True, (0, 0, 0)), False))
-wn = pygame.display.set_mode((500, 500)) # change later
 
 run = True
 while run:
@@ -64,8 +67,7 @@ while run:
                 data.close()
 
     wn.fill((255, 255, 255))
-    wn.blit(assets[0].asset, (0, 0))
-    print(assets[0].asset)
+    wn.blit(assets[0].asset, (assets[0].pos[0] + random.randint(-25, 25), assets[0].pos[1] + random.randint(-25, 25)))
     pygame.display.flip()
 
 pygame.quit()
