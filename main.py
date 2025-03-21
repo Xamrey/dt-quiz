8776ca8bbf66d7b0c23da58213828dd48a2842db
Xamrey <92014847+Xamrey@users.noreply.github.com>
2025-03-21 15:09:09 +1300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Stage: # Allows me to reference what menu is being used easier
     LearnBANDS = "__STAGE-LEARN-BANDS__"
     LearnLED = "__STAGE-LEARN_LED__"
     MenuPractice = "__STAGE-MENU-PRACTICE__"
-    PracticeBands = "__STAGE-PRACTICE-BANDS"
+    PracticeBands = "__STAGE-PRACTICE-BANDS__"
     PracticeLED = "__STAGE-PRACTICE-LED__"
 Stage = Stage() # Removing the need to create a copy of the class every time it is used
 
@@ -19,10 +19,12 @@ class Asset:
         self.pos = pos
         self.selectable = selectable
 
+# Setting up pygame and variables
 pygame.init()
 pygame.font.init()
 font = pygame.font.SysFont("Arial" ,24) # rename and change later (for various different text objects)
 wn = pygame.display.set_mode((500, 500)) # change later
+stage = Stage.MenuMain
 
 assets = [
     Asset(0, font.render("sample text", True, (0, 0, 0)), (0, 0), False)
@@ -34,7 +36,7 @@ def checkSelected():
 def createNotif(userCloses, message):
     print("change later", userCloses, message)
 
-practiceData = {
+practiceData = { # Default data
     "BANDS": {
         "CORRECT": 0,
         "INCORRECT": 0
@@ -56,19 +58,36 @@ except Exception as err:
     print(f"Error Code: '{err}'")
     createNotif(True, "There was an error in loading your data, any progress you make will not be saved (error code has been printed to the console)")
 
-run = True
-while run:
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            run = False
-            if saveData:
-                data = open("data.txt", "w")
-                data.write(json.dumps(practiceData))
-                data.close()
+def run(): # Putting the loop in a function allows it to be broken out of instantly
+    while True:
+        for i in pygame.event.get():
+            if i.type == pygame.QUIT:
+                if saveData:
+                    data = open("data.txt", "w")
+                    data.write(json.dumps(practiceData))
+                    data.close()
+                return
+            elif i.type == pygame.MOUSEBUTTONDOWN:
+                if i.button == 1:
+                    print("mouse1 down")
+            elif i.type == pygame.KEYDOWN:
+                if stage == Stage.PracticeBands or stage == Stage.PracticeLED:
+                    try:
+                        changevariablename = int(i.unicode)
+                        print("number key down")
+                    except:
+                        pass
 
-    wn.fill((255, 255, 255))
-    wn.blit(assets[0].asset, (assets[0].pos[0] + random.randint(-25, 25), assets[0].pos[1] + random.randint(-25, 25)))
-    pygame.display.flip()
+        wn.fill((255, 255, 255))
+        assets[0].pos = ((assets[0].pos[0] + 0.1) % 500, (assets[0].pos[1] + 0.05) % 500)
+        renderList = {0:[],1:[],2:[],3:[],4:[],5:[]}
+        for i in assets:
+            renderList[i.zIndex].append(i)
+        for i in renderList.values():
+            for v in i:
+                wn.blit(v.asset, v.pos)
+        pygame.display.flip()
 
+run()
 pygame.quit()
 pygame.font.quit()
